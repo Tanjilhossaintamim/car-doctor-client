d41c52bb1eabb14a76625c787813ae8e7901e396
Tanjil Hossain <tanjilhossain845@gmail.com>
2023-10-21 21:49:40 +0600

    compleate footer section

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/shared/Navbar/Navbar";
+import Footer from "../components/shared/Footer/Footer";
 
 export const Main = () => {
   return (
-    <div className="max-w-6xl mx-auto font-inter">
-      <Navbar />
-      <Outlet />
+    <div>
+      <div className="max-w-6xl mx-auto font-inter">
+        <Navbar />
+        <Outlet />
+      </div>
+      <div className="bg-color-dark-1">
+        <Footer />
+      </div>
     </div>
   );
 };
