be3a396df02febece7d83231ebb846798f1a281c
Tanjil Hossain <tanjilhossain845@gmail.com>
2023-10-22 15:48:36 +0600

    compleate time section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import About from "./About/About";
 import Banner from "./Banner/Banner";
 import Services from "./Services/Services";
+import Time from "./Time/Time";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Banner />
       <About />
       <Services />
+      <Time />
     </div>
   );
 };
